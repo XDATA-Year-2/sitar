a077137a229592aa04333a39dadb0e2bbf59e436
Roni Choudhury <roni.choudhury@kitware.com>
2014-08-13 13:27:35 -0400

    Got a basic grunt application workflow up and running

---
+++ Gruntfile.js
@@ -0,0 +1,88 @@
+/* global module */
+
+module.exports = function (grunt) {
+    "use strict";
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        jade: {
+            release: {
+                options: {
+                    data: {
+                        debug: false
+                    }
+                },
+                files: {
+                    "build/index.html": "jade/index.jade"
+                }
+            }
+        },
+        stylus: {
+            compile: {
+                options: {},
+                files: {
+                    "build/index.css": "styl/index.styl"
+                }
+            }
+        },
+        jshint: {
+            options: {
+                // Enforcing options (for strict checking, should be true by
+                // default; set to false indicates departure from this policy).
+                bitwise: true,
+                camelcase: true,
+                curly: true,
+                eqeqeq: true,
+                forin: true,
+                immed: true,
+                indent: 4,
+                latedef: true,
+                newcap: true,
+                noempty: false,
+                nonbsp: true,
+                nonew: true,
+                plusplus: false,
+                quotmark: "double",
+                undef: true,
+                unused: true,
+                strict: true,
+                maxparams: false,
+                maxdepth: false,
+                maxstatements: false,
+                maxcomplexity: false,
+                maxlen: false,
+
+                // Relaxing options (for strict checking, should be false by
+                // default; set to true indicates departure from this policy).
+                eqnull: true,
+
+                // Environment options.
+                browser: true
+            },
+            all: ["Gruntfile.js", "js/**/*.js"]
+        },
+        uglify: {
+            index: {
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                    "build/js/index.min.js": ["js/index.js"]
+                }
+            }
+        }
+    });
+
+    // Load plugins.
+    grunt.loadNpmTasks("grunt-contrib-jade");
+    grunt.loadNpmTasks("grunt-contrib-stylus");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+
+    // Default task.
+    grunt.registerTask("default", ["jade",
+                                   "stylus",
+                                   "jshint",
+                                   "uglify"]);
+};
---
+++ js/index.js
@@ -0,0 +1,9 @@
+/* global d3 */
+
+window.onload = function () {
+    "use strict";
+
+    d3.select("#gallery")
+        .append("h1")
+        .text("hello, world");
+};
